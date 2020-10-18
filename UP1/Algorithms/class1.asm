ccd82debbd1a4a44ea7c58ec9b0092fa61008cf4
Rubem Vasconcelos <rfsv@ic.ufal.br>
2020-10-18 20:13:05 -0300

    Update class1.asm

--- UP1/Algorithms/class1.asm
+++ UP1/Algorithms/class1.asm
@@ -1,3 +1,5 @@
+; Codigo em assembly pra setar um bit para a porta B, um pino configurado como saída
+
     #INCLUDE <P16F84A.inc>
     
     ORG 0x00
@@ -7,4 +9,13 @@ INICIO
     CLRW  ;Zera registrador de trabalho
     CLRF PORTA  ;Limpa porta A
     CLRF PORTB  ;Limpa porta B
-    ;Definição de estado
+    BSF STATUS,RP0 ;Registrador status setando no bip RP0
+    CLRF TRISA ;Zerar o primeiro registrador
+    CLRF TRISB ;Zerar o segundo registrador
+    BCF STATUS,RP0 ; Voltar pra o banco RP0 
+    
+MAIN
+    BSF PORTB,1 ;Seta o bit 1 na porta B
+    GOTO MAIN
+    
+    END
